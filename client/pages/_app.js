83f74c7f4cbac874cd205e3515fc5dd55d4551e3
Mohammad Mizanur Rahman <58907522+MOHAMMADmiZAN@users.noreply.github.com>
2022-11-01 14:34:44 +0600

    local Store solve

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,7 +2,7 @@ import GlobalStyles from '../components/styles/Global.styled';
 import '../styles/globals.css';
 import 'antd/dist/antd.css';
 import {StoreProvider} from 'easy-peasy';
-import store from '../store';
+import store from '../store/store';
 
 function MyApp({ Component, pageProps }) {
   return (
