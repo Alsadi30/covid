d2acab3d283bcb970d491bea473c16bdede53c2e
mohammadanarul <mdanarul7075@gmail.com>
2022-11-05 00:22:27 +0600

    update jwt token.

--- client/api/order.js
+++ client/api/order.js
@@ -1,13 +1,14 @@
-import { BackEndApi } from "./api"
+import { BackEndApi, jwtAuthToken } from "./api";
 
-  export const getOrders =async () => {
-  
-    try {
-        BackEndApi.defaults.headers.common['Authorization'] = `Bearer ${"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjY3MjcyMTAwLCJleHAiOjE2Njk4NjQxMDB9.u41PhQRIhKyaCzL68X-RVtEBfjWt2xWZb_A_leoMPhw"}`
-        const res = await BackEndApi.get(`/orders`)
-        const orders = res.data.data  
-        return orders     
-    } catch (e) {
-        console.log(e)
-    }
+export const getOrders = async () => {
+  try {
+    BackEndApi.defaults.headers.common[
+      "Authorization"
+    ] = `Bearer ${jwtAuthToken}`;
+    const res = await BackEndApi.get(`/orders`);
+    const orders = res.data.data;
+    return orders;
+  } catch (e) {
+    console.log(e);
   }
+};
--- client/components/productCard/product.jsx
+++ client/components/productCard/product.jsx
@@ -1,38 +1,38 @@
-import { useStoreActions, useStoreState } from 'easy-peasy';
-import { BsFillCartPlusFill } from 'react-icons/bs';
-import { FlexContentSpace } from '../styles/Flex.styled';
+import { useStoreActions, useStoreState } from "easy-peasy";
+import { BsFillCartPlusFill } from "react-icons/bs";
+import { FlexContentSpace } from "../styles/Flex.styled";
 
-const Product = ({product}) =>{
-   
-    const { AuthToken } = useStoreState((state) => state.Auth);
-    const {AddProductNoAuth,AddProductThunk} = useStoreActions(actions => actions.Cart);
+const Product = ({ product }) => {
+  const { AuthToken } = useStoreState((state) => state.Auth);
+  const { AddProductNoAuth, AddProductThunk } = useStoreActions(
+    (actions) => actions.Cart
+  );
 
-
-    const handleAddToCart = () =>{
-           if(AuthToken){
-            AddProductThunk(product)
-           }else{
-            // AddProductThunk(product)
-            AddProductNoAuth(product)
-           }
-        
+  const handleAddToCart = () => {
+    if (AuthToken) {
+      AddProductThunk(product);
+    } else {
+      // AddProductThunk(product)
+      AddProductNoAuth(product);
     }
+  };
 
-    return (
-       <>
-                        <img src={product?.attributes.thumbnails.data[0].attributes.url} alt={product?.attributes.name}></img>
-                        <FlexContentSpace>
-                            <div>
-                                <h4>{product?.attributes.name}</h4>
-                                <p>${product?.attributes.variants.data[0].attributes.sale_price}</p>
-                            </div>
-                            <div>
-                                <button onClick={handleAddToCart}><BsFillCartPlusFill size="35" color="#007580"></BsFillCartPlusFill></button>
-                            </div>
-                        </FlexContentSpace>
-                        </>
-                 
-    )
-}
+  return (
+    <>
+      {/* <img src={product?.attributes.thumbnails.data[0].attributes.url} alt={product?.attributes.name}></img> */}
+      <FlexContentSpace>
+        <div>
+          <h4>{product?.attributes.name}</h4>
+          <p>${product?.attributes.variants.data[0].attributes.sale_price}</p>
+        </div>
+        <div>
+          <button onClick={handleAddToCart}>
+            <BsFillCartPlusFill size="35" color="#007580"></BsFillCartPlusFill>
+          </button>
+        </div>
+      </FlexContentSpace>
+    </>
+  );
+};
 
-export default Product
+export default Product;
