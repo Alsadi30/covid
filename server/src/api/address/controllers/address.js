f9fcdc92ea4e87f52914b8ccfe183c0efa51b76a
Alsadi30 <abdullahsadi39@gmail.com>
2022-11-14 23:57:31 +0600

    backend ready for deployment

--- server/src/api/address/controllers/address.js
+++ server/src/api/address/controllers/address.js
@@ -1,9 +1,18 @@
-'use strict';
+'use strict'
 
 /**
  * address controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require('@strapi/strapi').factories
 
-module.exports = createCoreController('api::address.address');
+module.exports = createCoreController('api::address.address', ({ Strapi }) => ({
+  async create (ctx) {
+    const userId = ctx.state.user.id
+    ctx.request.body.data.user_id = userId
+    console.log(ctx.request.body)
+    const response = await super.create(ctx)
+    // some more logic
+    return response
+  }
+}))
